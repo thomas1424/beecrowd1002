6c781de60906f1b7eb0da992b4ae38d1a144da59
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2023-03-20 22:58:57 -0300

    Solução beecrowd 1002

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double A, n, R;
+            n = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A = n * Math.Pow(R, 2);
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+        }
+    }
+}
